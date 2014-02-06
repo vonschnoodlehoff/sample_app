5e201e624f5f5b0d879b673d27f92bdc91e2f6e4
vonschnoodlehoff <aschafer@y7mail.com>
2014-02-06 17:17:50 +1100

    Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 SampleApp2::Application.routes.draw do
-  get "users/new"
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
@@ -7,6 +6,7 @@ SampleApp2::Application.routes.draw do
   match '/home', to: 'static_pages#home', via: 'get'
   root  'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
