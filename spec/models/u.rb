ae2d92982b510dd4a3458a2be9a3c3b8de7e7d74
vonschnoodlehoff <aschafer@y7mail.com>
2014-02-06 17:28:31 +1100

    Nother add

---
+++ spec/models/u.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should be_valid }
+
+
+
+end
